33c202c333e5fe8cf9cd9f1f2281be52fe51cbb4
marwa sahraoui <marwasahrawi1988@gmail.com>
2022-11-20 18:55:01 +0100

    testing packets with differents conditions( no bomb /same amount of candies for each child /minimum 2 candies for each child

--- src/main/java/fr/arolla/Hollowin.java
+++ src/main/java/fr/arolla/Hollowin.java
@@ -1,19 +1,47 @@
 package fr.arolla;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class Hollowin {
 
+
     public static String showHolowinResponse(int children, List<List<String>> listOfPaquets) {
-        if(children > listOfPaquets.size()) {
+        if (children > listOfPaquets.size()) {
             return "Trick or treat!";
         }
 
-      for(List<String> paquet: listOfPaquets){
-        if(paquet.contains("bomb")){
-            return "Trick or treat!";
-         }
-       }
+        List<String> paquetFiltredOnlyCandy;
+        List<List<String>> bagFiltredOnlyCandy = new ArrayList<>();
+
+        int maxAmount = 2;
+
+        for (List<String> paquet : listOfPaquets) {
+
+            paquetFiltredOnlyCandy = new ArrayList<>();
+
+            for (int i = 0; i < paquet.size(); i++) {
+                if (paquet.size() < 2 || paquet.contains("bomb")) {
+                    return "Trick or treat!";
+                }
+                if (Objects.equals(paquet.get(i), "candy")) {
+                    paquetFiltredOnlyCandy.add(paquet.get(i));
+                }
+            }
+
+            bagFiltredOnlyCandy.add(paquetFiltredOnlyCandy);
+
+            if (maxAmount < paquetFiltredOnlyCandy.size()) {
+                maxAmount = paquetFiltredOnlyCandy.size();
+            }
+        }
+
+        for (List<String> paquet : bagFiltredOnlyCandy) {
+            if (paquet.size() < maxAmount) {
+                return "Trick or treat!";
+            }
+        }
 
         return "Thank you, strange uncle!";
     }
--- src/test/java/fr/arolla/HollowinTest.java
+++ src/test/java/fr/arolla/HollowinTest.java
@@ -3,46 +3,62 @@ package fr.arolla;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class HollowinTest {
- @Test
-    public void should_return_thankYou_when_3_children_have_3_paquets_with_2_candies_each_of_them(){
-     //given
-     List<String> packetsOfItems1 = List.of("candy","apple","candy");
-     List<String> packetsOfItems2 = List.of("candy","candy");
-     List<String> packetsOfItems3 = List.of("candy","candy");
-     List<List<String>> listOfPaquets = List.of(packetsOfItems1,packetsOfItems2,packetsOfItems3);
-     //when
+    @Test
+    public void should_return_thankYou_when_3_children_have_3_paquets_with_2_candies_each_of_them() {
+        //given
+        List<String> packetsOfItems1 = List.of("candy", "apple", "candy");
+        List<String> packetsOfItems2 = List.of("candy", "candy");
+        List<String> packetsOfItems3 = List.of("candy", "candy");
+        List<List<String>> listOfPaquets = List.of(packetsOfItems1, packetsOfItems2, packetsOfItems3);
+        //when
 
-     String result = Hollowin.showHolowinResponse(3,listOfPaquets);
-     //then
-     assertThat(result).isEqualTo("Thank you, strange uncle!");
- }
- @Test
- public void should_return_trick_or_treat_when_3_children_have_3_paquets_where_there_was_bumb(){
-  //given
-  List<String> packetsOfItems1 = List.of("candy","apple","candy");
-  List<String> packetsOfItems2 = List.of("candy","candy");
-  List<String> packetsOfItems3 = List.of("candy","bomb","candy");
-  List<List<String>> listOfPaquets = List.of(packetsOfItems1,packetsOfItems2,packetsOfItems3);
-  //when
+        String result = Hollowin.showHolowinResponse(3, listOfPaquets);
+        //then
+        assertThat(result).isEqualTo("Thank you, strange uncle!");
+    }
 
-  String result = Hollowin.showHolowinResponse(3,listOfPaquets);
-  //then
-  assertThat(result).isEqualTo("Trick or treat!");
-  }
- @Test
- public void should_return_trick_or_treat_when_3_children_have_2_paquets(){
-  //given
-  List<String> packetsOfItems1 = List.of("candy","apple","candy");
-  List<String> packetsOfItems2 = List.of("candy","candy");
-  List<List<String>> listOfPaquets = List.of(packetsOfItems1,packetsOfItems2);
-  //when
+    @Test
+    public void should_return_trick_or_treat_when_3_children_have_3_paquets_where_there_was_bumb() {
+        //given
+        List<String> packetsOfItems1 = List.of("candy", "apple", "candy");
+        List<String> packetsOfItems2 = List.of("candy", "candy");
+        List<String> packetsOfItems3 = List.of("candy", "bomb", "candy");
+        List<List<String>> listOfPaquets = List.of(packetsOfItems1, packetsOfItems2, packetsOfItems3);
+        //when
 
-  String result = Hollowin.showHolowinResponse(3,listOfPaquets);
-  //then
-  assertThat(result).isEqualTo("Trick or treat!");
- }
+        String result = Hollowin.showHolowinResponse(3, listOfPaquets);
+        //then
+        assertThat(result).isEqualTo("Trick or treat!");
+    }
 
+    @Test
+    public void should_return_trick_or_treat_when_3_children_have_2_paquets() {
+        //given
+        List<String> packetsOfItems1 = List.of("candy", "apple", "candy");
+        List<String> packetsOfItems2 = List.of("candy", "candy");
+        List<List<String>> listOfPaquets = List.of(packetsOfItems1, packetsOfItems2);
+        //when
+
+        String result = Hollowin.showHolowinResponse(3, listOfPaquets);
+        //then
+        assertThat(result).isEqualTo("Trick or treat!");
+    }
+
+    @Test
+    public void should_return_trick_or_treat_when_3_children_have_3_paquets_with_different_amount_of_candies() {
+        //given
+        List<String> packetsOfItems1 = List.of("candy", "apple", "candy");
+        List<String> packetsOfItems2 = List.of("candy", "candy");
+        List<String> packetsOfItems3 = List.of("candy", "candy", "candy");
+        List<List<String>> listOfPaquets = List.of(packetsOfItems1, packetsOfItems2, packetsOfItems3);
+        //when
+
+        String result = Hollowin.showHolowinResponse(3, listOfPaquets);
+        //then
+        assertThat(result).isEqualTo("Trick or treat!");
+    }
 }
